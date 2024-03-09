d2728c20d1ab99152d6c1f7ab3d1389597ef9b31
bhagyalaxmiai <161131605+bhagyalaxmiai@users.noreply.github.com>
2024-03-09 23:24:57 +0530

    data ingestion complete

--- source/component/train_data_ingestion.py
+++ source/component/train_data_ingestion.py
@@ -1,14 +1,16 @@
 import logging
 import os
 import pandas as pd
+from pandas import DataFrame
 from source.exception import ChurnException
 from pymongo.mongo_client import MongoClient
+from sklearn.model_selection import train_test_split
 from source.logger import logging
 
 
 class DataIngestion:
 
-    def __init__(self, train_config):
+    def __init__(self, train_config) -> DataFrame:
         self.train_config = train_config
 
     def export_data_into_feature_store(self):
@@ -19,7 +21,6 @@ class DataIngestion:
             # collection = self.train_config.collection_name
 
             client = MongoClient(self.train_config.mongodb_url_key)
-
             database = client[self.train_config.database_name]
             collection = database[self.train_config.collection_name]
 
@@ -29,22 +30,31 @@ class DataIngestion:
 
             dir_path = os.path.dirname(self.train_config.feature_store_dir_path)
             # print(f"feature_store_file_path {dir_path}")
-
             os.makedirs(dir_path, exist_ok=True)  # if not exist then create
-
             data.to_csv(self.train_config.feature_store_dir_path, index=False)
+
             logging.info("Complete : data load from mongodb")
+
+            return data
 
         except ChurnException as e:
             logging.error(e)
             raise e
 
-    def split_data_test_train(self):
+    def split_data_test_train(self, data: DataFrame) -> None:
         try:
-            pass
+            logging.info("start: train, test data split")
+            train_set, test_set = train_test_split(data, test_size=self.train_config.train_test_split_ratio)
+            dir_path = os.path.dirname(self.train_config.train_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+
+            train_set.to_csv(self.train_config.train_file_path, index=False)
+            test_set.to_csv(self.train_config.test_file_path, index=False)
+
+            logging.info("complete: train, test data split")
         except ChurnException as e:
             raise e
 
     def initiate_data_ingestion(self):
-        self.export_data_into_feature_store()
-        self.split_data_test_train()
+        data = self.export_data_into_feature_store()
+        self.split_data_test_train(data)
